f78d6b8d782ae7f40ca75b50b132458c87d5085c
sai1505 <vsai1505.bona@gmail.com>
2025-10-17 16:29:43 +0530

    Created SVGs.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:sample/svgs/sample_svg.dart';
 
 void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text("data"),
+          child: GeneratedSVG(),
         ),
       ),
     ),
