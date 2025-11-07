751568469ec9910270ce17bc7125d0314090c930
sai1505 <vsai1505.bona@gmail.com>
2025-11-07 18:50:34 +0530

    Creating a Sample SImple Application.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Title'),
+        ),
         body: Text("data"),
       ),
-    ),
-  );
+    );
+  }
 }
