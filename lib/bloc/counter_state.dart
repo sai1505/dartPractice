ec4012777082736627d143c987ed1d956de73f0f
sai1505 <vsai1505.bona@gmail.com>
2025-11-29 14:55:35 +0530

    Initial Setup

--- lib/bloc/counter_state.dart
+++
@@ -1,9 +0,0 @@
-part of 'counter_bloc.dart';
-
-@immutable
-sealed class CounterState {}
-
-final class CounterInitial extends CounterState {
-  final int value;
-  CounterInitial(this.value);
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:sample/bloc/counter_bloc.dart';
-import 'package:sample/home_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => CounterBloc(),
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: HomePage(),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: Text("data"),
       ),
     );
   }
